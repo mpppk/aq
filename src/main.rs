35089345fc97129adce0b01a68984fa45827cd3e
mpppk <niboshiporipori@gmail.com>
2017-02-20 22:55:37 +0900

    read json from stdin

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,17 @@ extern crate serde_json;
 use serde_json::Value;
 
 fn main() {
+
+    use std::io::{self, Read};
+
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+
     let parser = parser::parse_SelectorList(".foo[]").unwrap();
-    let data: Value = serde_json::from_str("{\"foo\": 13, \"bar\": \"baz\"}").unwrap();
+    let data: Value = serde_json::from_str(&buffer).unwrap();
+//    let data: Value = serde_json::from_str("{\"foo\": 13, \"bar\": \"baz\"}").unwrap();
     let adapter = match &parser[0] {
         &processor::Processor::Adapter(ref a) => a,
     };
-    println!("result: {:?}", adapter(&data).unwrap());
+    println!("{}", adapter(&data).unwrap().to_string());
 }
